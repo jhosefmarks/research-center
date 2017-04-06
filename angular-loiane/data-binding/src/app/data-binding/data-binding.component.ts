e048bdf2d0b205d5d5c5fad24c9406da08423b65
Jhosef Marks de Carvalho <jhosef.carvalho@totvs.com.br>
2017-04-06 17:01:50 -0300

    angular loiane data-binding

---
+++ angular-loiane/data-binding/src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.css']
+})
+export class DataBindingComponent implements OnInit {
+
+  url: string = 'http://jhosefmarks.training';
+  urlImagem: string = 'http://lorempixel.com/400/200/nature/';
+  cursoAngular: boolean = true;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  getValor () {
+    return 1;
+  }
+
+  getCurtirCurso () {
+    return true;
+  }
+}
