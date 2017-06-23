9126eff4f6e0990acf0ea24d529d4c2cc8650f48
Jhosef Marks de Carvalho <jhosef@gmail.com>
2017-06-23 11:12:37 -0300

    ts

--- typescript/alurabank/app/ts/app.ts
+++ typescript/alurabank/app/ts/app.ts
@@ -1,7 +1,4 @@
-let negociacao = new Negociacao(new Date(), 1, 100);
-
 const controller = new NegociacaoController();
 
-document
-  .querySelector('.form')
-  .addEventListener('submit', controller.adiciona.bind(controller));
+// Usando JQuery
+$('.form').submit(controller.adiciona.bind(controller));
