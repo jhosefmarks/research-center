1a1d35fcae5d11ab6d0ce4290adae502a1c69f4c
Jhosef Marks de Carvalho <jhosef@gmail.com>
2017-06-23 15:38:23 -0300

    ts api

--- typescript/alurabank/app/ts/controllers/NegociacaoController.ts
+++ typescript/alurabank/app/ts/controllers/NegociacaoController.ts
@@ -1,9 +1,16 @@
+import { domInject } from './../helpers/decorators/index';
+
 import { MensagemView, NegociacoesView } from './../views/index';
 import { Negociacao, Negociacoes } from './../models/index';
 
 export class NegociacaoController {
+  @domInject('#data')
   private _inputData: JQuery;
+
+  @domInject('#quantidade')
   private _inputQuantidade: JQuery;
+
+  @domInject('#valor')
   private _inputValor: JQuery;
 
   private _negociacoes = new Negociacoes();
@@ -12,9 +19,9 @@ export class NegociacaoController {
   private _negociacoesView = new NegociacoesView('#negociacoesView');
 
   constructor() {
-    this._inputData = $('#data');
+    /* this._inputData = $('#data');
     this._inputQuantidade = $('#quantidade');
-    this._inputValor = $('#valor');
+    this._inputValor = $('#valor'); */
 
     this._negociacoesView.update(this._negociacoes);
   }
@@ -22,8 +29,15 @@ export class NegociacaoController {
   adiciona(event: Event) {
     event.preventDefault();
 
+    let data = new Date(this._inputData.val().replace(/-/g, ','));
+
+    if (!this._ehDiaUtil(data)) {
+        this._mensagemView.update('Somente negociações em dias úteis, por favor!');
+        return
+    }
+
     const negociacao = new Negociacao(
-        new Date(this._inputData.val().replace(/-/g, ',')),
+        data,
         parseInt(this._inputQuantidade.val()),
         parseFloat(this._inputValor.val())
     );
@@ -33,4 +47,40 @@ export class NegociacaoController {
     this._negociacoesView.update(this._negociacoes);
     this._mensagemView.update('Negociação adicionada com sucesso');
   }
+
+  importarDados() {
+    function isOK(res: Response) {
+      if (res.ok) {
+        return res;
+      } else {
+        throw new Error(res.statusText);
+      }
+    }
+
+    fetch('http://localhost:8080/dados')
+      .then(res => isOK(res))
+      .then(res => res.json())
+      .then((dados: any[]) => {
+        dados
+          .map(dado => new Negociacao(new Date(), dado.ve, dado.montante))
+          .forEach(negociacao => this._negociacoes.adiciona(negociacao));
+
+        this._negociacoesView.update(this._negociacoes);
+      })
+      .catch(err => console.log(err.message));
+  }
+
+  private _ehDiaUtil(data: Date) {
+    return data.getDay() !== DiaDaSemana.Sabado && data.getDay() !== DiaDaSemana.Domingo;
+  }
+}
+
+enum DiaDaSemana {
+  Domingo,
+  Segunda,
+  Terca,
+  Quarta,
+  Quinta,
+  Sexta,
+  Sabado
 }
--- typescript/alurabank/app/ts/views/View.ts
+++ typescript/alurabank/app/ts/views/View.ts
@@ -1,3 +1,5 @@
+// import { logarTempoDeExecucao } from '../helpers/decorators/index';
+
 export abstract class View<T> {
   private _elemento: JQuery;
   private _escapar: boolean;
@@ -7,6 +9,7 @@ export abstract class View<T> {
     this._escapar = escapar;
   }
 
+  // @logarTempoDeExecucao()
   update(model: T) {
     let template = this.template(model);
 
